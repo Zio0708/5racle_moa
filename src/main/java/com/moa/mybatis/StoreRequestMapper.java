723787f44c778247e6a83a16d0f424c2ab6f303f
DragonHee <50479743+DragonHee@users.noreply.github.com>
2019-08-07 22:06:43 +0900

    Add files via upload
    v0.77

--- src/main/java/com/moa/mybatis/StoreRequestMapper.java
+++ src/main/java/com/moa/mybatis/StoreRequestMapper.java
@@ -1,15 +1,14 @@
 package com.moa.mybatis;
 
+import com.moa.model.vo.ReadStoreRequestVO;
 import com.moa.model.vo.RequestProductVO;
-import com.moa.model.vo.SimpleHostRequestVO;
 import com.moa.model.vo.SimpleUserRequestVO;
 
 import java.util.List;
-import java.util.Map;
 
 public interface StoreRequestMapper {
+    List<String> searchRequestPicture(int requestId);
+    ReadStoreRequestVO searchRequestInfo(int requestId);
     List<SimpleUserRequestVO> searchRequestList(int userId);
     List<RequestProductVO> searchRequestProduct(int requestId);
-    List<SimpleHostRequestVO> searchListByHost(Map<String,Object> map);
-    int searchAllListCnt(Map<String,Object> map);
 }
