e8e96743bfce85e086556ea07deac8320fcc4ff1
chlalstjd <chlalstjd430@naver.com>
2019-08-10 22:50:17 +0900

    update by choiminseong

--- src/main/java/com/moa/controller/MyPageController.java
+++ src/main/java/com/moa/controller/MyPageController.java
@@ -3,12 +3,19 @@ package com.moa.controller;
 
 import com.moa.model.service.LuggageRequestInfoService;
 import com.moa.model.service.LuggageRequestRecordService;
+import com.moa.model.service.MessengerListServiceImpl;
+import com.moa.model.vo.MessageVO;
 import com.moa.model.vo.ReadStoreRequestVO;
+import com.moa.paging.Pagination;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -18,6 +25,8 @@ public class MyPageController {
     private LuggageRequestRecordService luggageRequestRecordService;
     @Autowired
     private LuggageRequestInfoService luggageRequestInfoService;
+    @Autowired
+    private MessengerListServiceImpl messengerListService;
 
     @RequestMapping(value="", method= RequestMethod.GET)
     public ModelAndView myPage() {
@@ -60,6 +69,130 @@ public class MyPageController {
         ModelAndView mav=new ModelAndView();
         mav.setViewName("/error/page");
         mav.addObject("message","존재하지 않는 페이지 입니다.");
+        return mav;
+    }
+    @RequestMapping(value = {"/message/receive","/message"})
+    public ModelAndView message(HttpServletRequest request){
+        HttpSession session = request.getSession();//추후 로그인 처리
+        int userId = 7;
+        ModelAndView mav = new ModelAndView();
+
+        //--list logic
+        Map<String,Object> messageInfo = new HashMap<String,Object>();
+        messageInfo.put("messageType",MessengerStateMessage.MESSAGE_TYPE_RECEIVER);
+        messageInfo.put("userId",userId);
+        messageInfo.put("firstNum",1);
+        messageInfo.put("lastNum",10);
+
+        List<MessageVO> list = messengerListService.messageList(messageInfo);
+        int allListCnt = messengerListService.messageListCnt(messageInfo);
+
+        //--paging logic
+        Pagination pagination = new Pagination(allListCnt,1,list.size());
+
+        //ModelAndView
+        mav.setViewName("messageReceive");
+        mav.addObject(list);
+        mav.addObject(pagination);
+
+        System.out.println(mav);
+
+        return mav;
+    }
+    @RequestMapping("/message/receive/{curPage}")
+    public ModelAndView messageCurPage(HttpServletRequest request,
+                                       @PathVariable int curPage){
+        HttpSession session = request.getSession();//추후 로그인 처리
+        int userId = 7;
+        ModelAndView mav = new ModelAndView();
+
+        //--list logic
+        Map<String,Object> messageInfo = new HashMap<String,Object>();
+        messageInfo.put("messageType",MessengerStateMessage.MESSAGE_TYPE_RECEIVER);
+        messageInfo.put("userId",userId);
+        messageInfo.put("firstNum",(curPage-1)*10+1);
+        messageInfo.put("lastNum",(curPage-1)*10+10);
+
+        List<MessageVO> list = messengerListService.messageList(messageInfo);
+        int allListCnt = messengerListService.messageListCnt(messageInfo);
+
+        //--paging logic
+        Pagination pagination = new Pagination(allListCnt,1,list.size());
+
+        //ModelAndView
+        mav.setViewName("messageReceive");
+        mav.addObject(list);
+        mav.addObject(pagination);
+
+        System.out.println(mav);
+
+        return mav;
+    }
+
+    //보낸 메세지
+    @RequestMapping("/message/send")
+    public ModelAndView messageSend(HttpServletRequest request){
+        HttpSession session = request.getSession();//추후 로그인 처리
+        int userId = 7;
+        ModelAndView mav = new ModelAndView();
+
+        //--list logic
+        Map<String,Object> messageInfo = new HashMap<String,Object>();
+        messageInfo.put("messageType", MessengerStateMessage.MESSAGE_TYPE_SENDER);
+        messageInfo.put("userId",userId);
+        messageInfo.put("firstNum",1);
+        messageInfo.put("lastNum",10);
+
+        List<MessageVO> list = messengerListService.messageList(messageInfo);
+        int allListCnt = messengerListService.messageListCnt(messageInfo);
+
+        //--paging logic
+        Pagination pagination = new Pagination(allListCnt,1,list.size());
+
+        //ModelAndView
+        mav.setViewName("messageSend");
+        mav.addObject(list);
+        mav.addObject(pagination);
+
+        System.out.println(mav);
+
+        return mav;
+    }
+
+    //메시지 상세보기
+    @RequestMapping("/message/send/detail/{messageNum}")
+    public ModelAndView messageSendDetail(@PathVariable int messageNum){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("messageDetail");
+        MessageVO msg = messengerListService.messageDetail(messageNum);
+        mav.addObject("messageInfo",msg);
+        mav.addObject("messageType","send");
+        return mav;
+    }
+    @RequestMapping("/message/receive/detail/{messageNum}")
+    public ModelAndView messageReceiveDetail(@PathVariable int messageNum){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("messageDetail");
+        MessageVO msg = messengerListService.messageDetail(messageNum);
+        mav.addObject("messageInfo",msg);
+        mav.addObject("messageType","receive");
+        return mav;
+    }
+    @RequestMapping(value = "/message/submit", method = RequestMethod.POST)
+    public ModelAndView messageSubmitReceiver(@RequestParam String receiverNick){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("messageSendDetail");
+        mav.addObject("receiveNick", receiverNick);
+        //SESSION에 있는 닉네임 정보
+        mav.addObject("userNick","성진킴");
+        return mav;
+    }
+    @RequestMapping(value = "/message/submit", method = RequestMethod.GET)
+    public ModelAndView messageSubmit(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("messageSendDetail");
+        //SESSION에 있는 닉네임 정보
+        mav.addObject("userNick","성진킴");
         return mav;
     }
 }
---
+++ src/main/java/com/moa/message/MessengerStateMessage.java
@@ -0,0 +1,8 @@
+package com.moa.message;
+
+public class MessengerStateMessage {
+    public static final String READ ="read";
+    public static final String UNREAD = "unread";
+    public static final String MESSAGE_TYPE_SENDER = "send";
+    public static final String MESSAGE_TYPE_RECEIVER = "receive";
+}
