3649534e9a9b17c9de4980da668560e8544c8b8b
JoonHee <mornings9047@naver.com>
2019-08-10 21:37:24 +0900

    송준희 - '보관해드립니다' 글 등록 DAO 인터페이스

--- src/main/java/com/moa/model/dao/StoreBoardDAOImpl.java
+++ src/main/java/com/moa/model/dao/StoreBoardDAOImpl.java
@@ -1,101 +1,80 @@
 package com.moa.model.dao;
 
-import com.moa.model.vo.*;
+import com.moa.model.vo.SimpleStorageAddressVO;
+import com.moa.model.vo.StoreBoardFormVO;
 import com.moa.mybatis.StoreBoardMapper;
+import lombok.NoArgsConstructor;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+import sun.java2d.pipe.SpanShapeRenderer;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 @Repository
+@NoArgsConstructor
 public class StoreBoardDAOImpl implements StoreBoardDAO {
     @Autowired
+    @Qualifier("sqlSession_oracle")
     private SqlSession sqlSession_oracle;
+
+    /*init*/
     @Override
-    public List<EntrustSearchVO> searchEntrust(DetailOptionVO detail){
-        System.out.println(detail.getLatitude());
+    public List<SimpleStorageAddressVO> selectStorageAddress(String hostId) {
         StoreBoardMapper mapper = sqlSession_oracle.getMapper(StoreBoardMapper.class);
-        System.out.println(mapper);
-        List<EntrustSearchVO> entrustAry = mapper.searchEntrust(detail);
-        System.out.println("DAO 완료");
-        return entrustAry;
+        List<SimpleStorageAddressVO> storage = mapper.selectStorages(Integer.parseInt(hostId));
+        return storage;
     }
     @Override
-    public StoreBoardVO searchOne(int articleNum) {
-        StoreBoardMapper mapper=sqlSession_oracle.getMapper(StoreBoardMapper.class);
-        Map<String,Object> map=mapper.selectOne(articleNum);
-        StoreBoardVO vo=new StoreBoardVO();
-
-        vo.setArticleNum(articleNum);
-        vo.setTitle((String)map.get("제목"));
-        vo.setContent((String)map.get("내용"));
-        vo.setWriteDate(((java.sql.Timestamp)map.get("작성일자")).toString());
-        vo.setTransactionType((String)map.get("거래방식"));
-        vo.setStorageType((String)map.get("보관지형태"));
-        vo.setStoragePeriodType((String)map.get("보관기간"));
-        vo.setBaseAddress((String)map.get("기본주소"));
-        vo.setDetailAddress((String)map.get("상세주소"));
-        vo.setForbiddenProduct((String)map.get("보관금지물품"));
-        vo.setSecurityFacility((String)map.get("보안시설"));
-        vo.setPet((String)map.get("펫"));
-        vo.setPictureName(Arrays.asList(((String)map.get("사진명")).split(",")));
-        List<String> detailPriceNameList = mapper.selectDetailPrice();
-        List<String> detailPriceList=Arrays.asList(((String)map.get("상세가격")).split(","));
-        Map<String,Integer> detailPriceMap=new HashMap<String,Integer>();
-        for(int i=0;i<detailPriceNameList.size();i++)
-            detailPriceMap.put(detailPriceNameList.get(i), Integer.parseInt(detailPriceList.get(i)));
-        vo.setDetailPrice(detailPriceMap);
-        return vo;
+    public List<String> selectTransaction() {
+        StoreBoardMapper mapper = sqlSession_oracle.getMapper(StoreBoardMapper.class);
+        List<String> transactionList = mapper.selectTransaction();
+        return transactionList;
     }
     @Override
-    public List<SimpleStorageBoardVO> searchMyStorage(Map<String, Object> storageInfo){
+    public List<String> selectCategory() {
         StoreBoardMapper mapper = sqlSession_oracle.getMapper(StoreBoardMapper.class);
-
-        return mapper.searchMyStorage(storageInfo);
+        List<String> categoryList = mapper.selectCategory();
+        return categoryList;
     }
     @Override
-    public int searchMyStorageCnt(int hostId){
+    public List<String> selectStoragePeriod() {
         StoreBoardMapper mapper = sqlSession_oracle.getMapper(StoreBoardMapper.class);
-
-        return mapper.searchMyStorageCnt(hostId);
+        List<String> periodList = mapper.selectStoragePeriod();
+        return periodList;
+    }
+    @Override
+    public List<String> selectPrice() {
+        StoreBoardMapper mapper = sqlSession_oracle.getMapper(StoreBoardMapper.class);
+        List<String> priceList = mapper.selectPrice();
+        return priceList;
     }
 
     @Override
     public int insert(StoreBoardFormVO storeBoardFormVO) {
-        System.out.println("DAO: " + storeBoardFormVO);
         StoreBoardMapper mapper = sqlSession_oracle.getMapper(StoreBoardMapper.class);
-        int i = 0, articleNum = 0, transaction = 0, cctv = 0, storagePeriod = 0;
-        int categoryId = 0;
+        int i, articleNum, transaction, cctv, storagePeriod, categoryId;
         String pet = null;
-
-        Map<String, Object> parameters = new HashMap<>();  // �쟾�넚�슜
-        Map<String, Object> result = null;      // return �슜
+        Map<String, Object> parameters = new HashMap<>();  // 전송용
+        Map<String, Object> result = null;      // return 용
 
         /*selectParameters*/
-
-        long startTime = System.nanoTime();
         parameters.put("transaction", storeBoardFormVO.getTransactionType());
         parameters.put("cctv", storeBoardFormVO.getCctv());
         parameters.put("storagePeriod", storeBoardFormVO.getStoragePeriodType());
-        result = mapper.selectParameters(parameters);
-        long endTime = System.nanoTime();
-        System.out.println("1: "+ (endTime-startTime)/1000000000);
 
-//        result.forEach((k,v)-> System.out.println(k + "/" + v));
+        result = mapper.selectParameters(parameters);
         articleNum = ((BigDecimal) result.get("NEXTVAL")).intValue();
         transaction = ((BigDecimal) result.get("SR_TRANSACTION_TYPE_ID")).intValue();
         cctv = ((BigDecimal) result.get("SECURITY_FACILITY_ID")).intValue();
         storagePeriod = ((BigDecimal) result.get("STORAGE_PERIOD_TYPE_ID")).intValue();
 
-
         /*insertIntoStoreBoard*/
-        startTime = System.nanoTime();
         parameters.clear();
         parameters.put("articleNum", articleNum);
         parameters.put("hostId", storeBoardFormVO.getHostId());
@@ -105,38 +84,23 @@ public class StoreBoardDAOImpl implements StoreBoardDAO {
         parameters.put("content", storeBoardFormVO.getContent());
         parameters.put("storageId", storeBoardFormVO.getStorageId());
         mapper.insertIntoStoreBoard(parameters);
-//        System.out.println(parameters);
-        endTime = System.nanoTime();
-        System.out.println("2: "+ (endTime-startTime)/1000000000);
-
 
         /*insertIntoPetType*/
-        startTime = System.nanoTime();
         parameters.clear();
         pet = storeBoardFormVO.getPet();
         if(pet!=null) {
             parameters.put("articleNum", articleNum);
             parameters.put("pet", pet);
-//        System.out.println(parameters);
             mapper.insertIntoPetType(parameters);
-//        System.out.println("printed");
-            endTime = System.nanoTime();
-            System.out.println("3: " + (endTime - startTime) / 1000000000);
         }
 
         /*insertIntoSF_SB*/
-        startTime = System.nanoTime();
         parameters.clear();
         parameters.put("cctv", cctv);
         parameters.put("articleNum", articleNum);
         mapper.insertIntoSF_SB(parameters);
-//        System.out.println(parameters);
-        endTime = System.nanoTime();
-        System.out.println("4: "+ (endTime-startTime)/1000000000);
-
 
         /*insertIntoSBAttachedPicture*/
-        startTime = System.nanoTime();
         parameters.clear();
         parameters.put("articleNum", articleNum);
         List<String> list = storeBoardFormVO.getPictureName();
@@ -145,29 +109,26 @@ public class StoreBoardDAOImpl implements StoreBoardDAO {
             parameters.put("pictureName", list.get(i));
             mapper.insertIntoSBAttachedPicture(parameters);
         }
-//        System.out.println(parameters);
-        endTime = System.nanoTime();
-        System.out.println("5: "+ (endTime-startTime)/1000000000);
-
-
 
         /*insertIntoDetailPriceFibo*/
-        startTime = System.nanoTime();
         parameters.clear();
         parameters.put("articleNum", articleNum);
         list = storeBoardFormVO.getPrice();
         length = list.size();
         for (i = 0; i < length; i++) {
             parameters.put("priceId", i + 1);
-            parameters.put("price", list.get(i));
+            String price = "";
+            if(list.get(i).length()>3) {
+                StringTokenizer tokenizer = new StringTokenizer(list.get(i), ",");
+                while(tokenizer.hasMoreTokens()) {
+                    price+=tokenizer.nextToken();
+                }
+            }
+            parameters.put("price", price);
             mapper.insertIntoDetailPriceFibo(parameters);
         }
-//        System.out.println(parameters);
-        endTime = System.nanoTime();
-        System.out.println("6: "+ (endTime-startTime)/1000000000);
 
         /*insertIntoForbiddenProduct*/
-        startTime = System.nanoTime();
         parameters.clear();
         parameters.put("articleNum", articleNum);
         list = storeBoardFormVO.getForbiddenCategoryList();
@@ -179,9 +140,6 @@ public class StoreBoardDAOImpl implements StoreBoardDAO {
             parameters.put("productName", list2.get(i));
             mapper.insertIntoForbiddenProduct(parameters);
         }
-
-        endTime = System.nanoTime();
-        System.out.println("7: "+ (endTime-startTime)/1000000000);
         return articleNum;
     }
 }
