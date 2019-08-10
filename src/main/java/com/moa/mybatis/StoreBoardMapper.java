edf7b68de2c0da259a243f3d898ddde4ee097fc4
JoonHee <mornings9047@naver.com>
2019-08-10 21:50:10 +0900

    송준희 - '보관해드립니다' Mapper

--- src/main/java/com/moa/mybatis/StoreBoardMapper.java
+++ src/main/java/com/moa/mybatis/StoreBoardMapper.java
@@ -1,19 +1,17 @@
 package com.moa.mybatis;
 
-import com.moa.model.vo.DetailOptionVO;
-import com.moa.model.vo.EntrustResultSearchVO;
-import com.moa.model.vo.EntrustSearchVO;
-import com.moa.model.vo.SimpleStorageBoardVO;
+import com.moa.model.vo.SimpleStorageAddressVO;
 
 import java.util.List;
 import java.util.Map;
 
 public interface StoreBoardMapper {
-    Map<String,Object> selectOne(int articleNum);
-    List<String> selectDetailPrice();
-    List<EntrustSearchVO> searchEntrust(DetailOptionVO detail);
-    List<SimpleStorageBoardVO> searchMyStorage(Map<String,Object> storageInfo);
-    int searchMyStorageCnt(int hostId);
+    List<SimpleStorageAddressVO> selectStorages(int hostId);
+    List<String> selectTransaction();
+    List<String> selectCategory();
+    List<String> selectStoragePeriod();
+    List<String> selectPrice();
+
     Map<String, Object> selectParameters(Map<String, Object> map);
     int insertIntoStoreBoard(Map<String, Object> map);
     int insertIntoPetType(Map<String, Object> map);
