ed67a7eef0bcfb1bc3c527a3b51d77ce7a981aed
JoonHee <mornings9047@naver.com>
2019-08-10 21:20:27 +0900

    송준희-'보관해드립니다' 글 등록 페이지로 넘어오는 controller

--- src/main/java/com/moa/controller/KeepController.java
+++ src/main/java/com/moa/controller/KeepController.java
@@ -1,17 +1,29 @@
 package com.moa.controller;
 
+import com.moa.model.service.LuggageWelcomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-@RequestMapping("/storeboard")
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RequestMapping("/storeBoard")
 @Controller
-public class KeepConxtroller {
-	@RequestMapping("/keep")
-	public String registerStoreBoard() {
-		return "keep";
-	}
-	public void asd(){
+public class KeepController {
+	@Autowired
+	private LuggageWelcomeService luggageWelcomeService;
 
+	@RequestMapping("/keep")
+	public ModelAndView registerStoreBoard(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("keep");
+
+		String hostId = "3"; //String hostId = (String)request.getAttribute("hostId");
+		mav.addObject("hostId", hostId);
+		Map<String, Object> map = luggageWelcomeService.initBoard(hostId);
+		mav.addObject("map", map);
+		return mav;
 	}
 }
